2ca4d25241ac04eed81cbb3ea73d14dc82d2a1c6
Michael Eliachevitch <m.eliachevitch@posteo.de>
2023-01-25 04:12:11 +0100

    Start exercises

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,9 @@
 // variables2.rs
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
-    if x == 10 {
+    let x : u8 = 0b110;
+    if x == 10{
         println!("x is ten!");
     } else {
         println!("x is not ten!");
